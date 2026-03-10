6654c566acc1914f40f82f1383f67c788f2b129b
Roberto de Farias <235542534+robertodefarias@users.noreply.github.com>
2026-03-10 15:41:22 -0300

    Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  root "lists#index"
   resources :lists, only: [ :index, :show, :new, :create ] do
     resources :bookmarks, only: [ :new, :create ]
   end
